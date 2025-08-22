6eb4440835f6301c2faf732dbd391794fd5d15e9
Fernando May <fmay130@gmail.com>
2025-08-22 11:03:45 -0600

    prev code

--- contracts/hello-world/src/lib.rs
+++ contracts/hello-world/src/lib.rs
@@ -21,3 +21,25 @@ impl Contract {
 }
 
 mod test;
+
+// #![no_std]
+// use soroban_sdk::{contract, contractimpl, Env, Symbol};
+
+// #[contract]
+// pub struct CounterContract;
+
+// #[contractimpl]
+// impl CounterContract {
+//     pub fn increment(env: Env) -> u32 {
+//         let key = Symbol::short("count");
+//         let mut count: u32 = env.storage().persistent().get(&key).unwrap_or(0);
+//         count += 1;
+//         env.storage().persistent().set(&key, &count);
+//         count
+//     }
+
+//     pub fn get(env: Env) -> u32 {
+//         let key = Symbol::short("count");
+//         env.storage().persistent().get(&key).unwrap_or(0)
+//     }
+// }
